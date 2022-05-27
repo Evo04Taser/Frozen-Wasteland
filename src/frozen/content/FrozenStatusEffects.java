43ffe037f2539d37a605477d71761cb92945de69
Evo04Taser <104289345+Evo04Taser@users.noreply.github.com>
2022-05-27 17:37:27 +0800

    Create FrozenStatusEffects.java

---
+++ src/frozen/content/FrozenStatusEffects.java
@@ -0,0 +1,19 @@
+import arc.*;
+
+import arc.graphics.*;
+
+import arc.math.*;
+
+import mindustry.ctype.*;
+
+import mindustry.game.*;
+
+import mindustry.game.EventType.*;
+
+import mindustry.type.*;
+
+import mindustry.graphics.*;
+import static mindustry.Vars*;
+public class StatusEffects implements ContentList{
+
+    public static StatusEffect
