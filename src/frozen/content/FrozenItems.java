a33343704b7207379abae1fba3e723e876cba1aa
Evo04Taser <104289345+Evo04Taser@users.noreply.github.com>
2022-05-28 09:57:34 +0800

    Add files via upload

---
+++ src/frozen/content/FrozenItems.java
@@ -0,0 +1,27 @@
+package frozen.content;
+
+import arc.graphics.*;
+import mindustry.ctype.*;
+import mindustry.type.*;
+
+public class Items implements ContentList{
+    public static Item
+    snow, snow-block, snow-brick;
+
+    @Override
+    public void load(){
+        snow = new Item("snow", Color.valueOf("e8e8e8")){{
+            hardness = 0.3;
+            cost = 0.7f;
+            alwaysUnlocked = true;
+        }};
+
+        snowBlock = new Item("snow-block", Color.valueOf("f5f5f5")){{
+            cost = 1f;
+        }};
+
+        snowBrick = new Item("snow-brick", Color.valueOf("ffffff")){{
+            cost = 1.5f;
+        }}
+    }
+}
