83c4c95383abbd057d575cba77470b1e97c72733
Evo04Taser <104289345+Evo04Taser@users.noreply.github.com>
2022-05-28 09:34:05 +0800

    Add files via upload

--- src/frozen/world/blocks/power/ChargeGenerator.java
+++ src/frozen/world/blocks/power/ChargeGenerator.java
@@ -4,5 +4,16 @@ import mindustry.type.*;
 import mindustry.world.meta.*;
 
 public class ChargeGenerator extends ItemLiquidGenerator{
-  
+
     public ChargeGenerator(String name){
+        super(true, false, name);
+        hasItems = true;
+        hasLiquids = false;
+        envEnabled = Env.any;
+    }
+
+    @Override
+    protected float getItemEfficiency(Item item){
+        return item.charge;
+    }
+}
