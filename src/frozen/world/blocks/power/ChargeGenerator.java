b8389e1df2552bcf289e69aab906c9137d315495
Evo04Taser <104289345+Evo04Taser@users.noreply.github.com>
2022-05-28 09:30:35 +0800

    Create ChargeGenerator.java

---
+++ src/frozen/world/blocks/power/ChargeGenerator.java
@@ -0,0 +1,8 @@
+package frozen.world.blocks.power;
+
+import mindustry.type.*;
+import mindustry.world.meta.*;
+
+public class ChargeGenerator extends ItemLiquidGenerator{
+  
+    public ChargeGenerator(String name){
