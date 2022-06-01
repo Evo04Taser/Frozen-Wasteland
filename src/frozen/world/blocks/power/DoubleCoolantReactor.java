e21b978ccb6c5f5af130de6d38a9c3dd104152ad
Evo04Taser <104289345+Evo04Taser@users.noreply.github.com>
2022-06-01 17:00:32 +0800

    Create DoubleCoolantReactor.java

---
+++ src/frozen/world/blocks/power/DoubleCoolantReactor.java
@@ -0,0 +1,38 @@
+package frozen.world.blocks.power;
+
+import arc.*;
+import arc.graphics.*;
+import arc.graphics.g2d.*;
+import arc.math.*;
+import arc.struct*;
+import arc.util*;
+import arc.util.io*;
+import mindustry.annotations.Annotations.*;
+import mindustry.content.*;
+import mindustry.entities.*;
+import mindusty.game.EventType.*;
+import mindusty.gen*;
+import mindusty.graphics.*;
+import mindustry.logic.*;
+import mindustry.ui.*;
+import mindustry.world.meta.*;
+
+import static mindustry.Vars.*;
+
+public class DoubleCoolantReactor extends PowerGenerator{
+    public float itemDurarion = 60f;
+    public int explosionRadius = 25f;
+    public int explosionDamage = 2100f;
+    public Effect explodeEffect = Fx.impactReactorExplosion;
+    public Liquid coolant = Liquids.cryofluid;
+    public float coolantUsage = 0.2f;
+    public float powerUse = 3f;
+    public float itemDuration = 120f;
+    public int itemCapacity = 20f;
+    public int liquidCapacity = 50f;
+
+    public @Load("@-top") TextureRegion topRegion;
+
+    public DoubleCoolantReactor(String name){
+        super(name);
+        rebuildable = false
