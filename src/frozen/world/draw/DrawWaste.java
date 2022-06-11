55435474cffc9d22a51a8fcc96bd9d4d8eba9feb
Eatyourdust <104289345+Evo04Taser@users.noreply.github.com>
2022-06-11 13:17:02 +0800

    Update DrawWaste.java

--- src/frozen/world/draw/DrawWaste.java
+++ src/frozen/world/draw/DrawWaste.java
@@ -1,1 +1,10 @@
-/** WIP(WORK IN PROGRESS) */
+/** WIP(WORK IN PROGRESS) 
+package frozen.world.draw;
+
+import arc.*;
+import arc.graphics.*;
+import arc.graphics.g2d,*;
+import arc.math.*;
+import arc.util.*;
+import mindustry.gen.*;
+import mindustry.graphics.*; */
