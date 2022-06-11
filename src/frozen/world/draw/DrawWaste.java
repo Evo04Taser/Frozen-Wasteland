5da6248f1eb83f1cc43df6320de79e40b89fb53d
Eatyourdust <104289345+Evo04Taser@users.noreply.github.com>
2022-06-11 14:02:29 +0800

    Update DrawWaste.java

--- src/frozen/world/draw/DrawWaste.java
+++ src/frozen/world/draw/DrawWaste.java
@@ -7,4 +7,28 @@ import arc.graphics.g2d,*;
 import arc.math.*;
 import arc.util.*;
 import mindustry.gen.*;
-import mindustry.graphics.*; */
+import mindustry.graphics.*;
+import mindustry.world.*;
+import mindustry.world.blocks.power.PowerGenerator.*;
+import mindustry.world.draw.*;
+
+public class DrawWaste extends DrawBlock {
+    public Item waste = Items.graphite;
+    public @Nullable Liquid liquidWaste;
+    public TextureRegion wasteRegion;
+
+    @Override
+    public void setStats(PowerGeneratorBuild build){
+      super.setStats();
+
+      if(consumesItems){
+         stats.add(Stat.output, StatValues.items(itemDuration / 60f, waste));
+      }
+    }
+
+    @Override
+    public void init(){
+      outputsItem = waste != null;
+      outputsLiquid = liquidWaste != null;
+      super.init();
+    } */
