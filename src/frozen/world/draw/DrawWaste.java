72b206ae0a3052e1cc88bb4738bff8a7e2d6cb9a
Eatyourdust <104289345+Evo04Taser@users.noreply.github.com>
2022-06-11 15:25:44 +0800

    Correction/s(again)

--- src/frozen/world/draw/DrawWaste.java
+++ src/frozen/world/draw/DrawWaste.java
@@ -2,7 +2,7 @@ package frozen.world.draw;
 
 import arc.*;
 import arc.graphics.*;
-import arc.graphics.g2d,*;
+import arc.graphics.g2d.*;
 import arc.math.*;
 import arc.util.*;
 import mindustry.gen.*;
