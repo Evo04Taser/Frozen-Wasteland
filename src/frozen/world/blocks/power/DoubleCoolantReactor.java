f0453ad299994e97ae638b4007166d8fcaf14294
Eatyourdust <104289345+Evo04Taser@users.noreply.github.com>
2022-07-02 15:41:36 +0800

    Update DoubleCoolantReactor.java

--- src/frozen/world/blocks/power/DoubleCoolantReactor.java
+++ src/frozen/world/blocks/power/DoubleCoolantReactor.java
@@ -92,6 +92,8 @@ public class DoubleCoolantReactor extends PowerGenerator{
         () -> Pal.powerBar,
         () -> Pal.powerProductionMultiplier,
         () -> entity.productionEfficiency,
+        )                                                        
+      )                                              
     }
 
     @Override
